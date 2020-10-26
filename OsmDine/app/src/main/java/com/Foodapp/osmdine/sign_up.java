689514914f5875ea0cc2f69f2c4cf13643cbbd2c
JAYESH BUDHWANI <54534558+Jay0774@users.noreply.github.com>
2020-10-26 16:33:05 +0530

    osmidine with signin and signup

---
+++ OsmDine/app/src/main/java/com/Foodapp/osmdine/sign_up.java
@@ -0,0 +1,39 @@
+package com.Foodapp.osmdine;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class sign_up extends AppCompatActivity {
+
+    EditText editPhone, editName, editPassword;
+    Button btnSignUp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+        editName = findViewById(R.id.editName);
+        editPhone = findViewById(R.id.editPhone);
+        editPassword = findViewById(R.id.editPassord);
+        btnSignUp = findViewById(R.id.btnSignUp);
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(editPhone.getText().toString().equals("9079320380") &&
+                        editPassword.getText().toString().equals("admin")) {
+                    Toast.makeText(getApplicationContext(),
+                            "Redirecting...",Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(getApplicationContext(), "Wrong Credentials",Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        });
+    }
+}
