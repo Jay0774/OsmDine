36b7c53c85b34c22d4b817e83b20349b2df5e287
JAYESH BUDHWANI <54534558+Jay0774@users.noreply.github.com>
2020-12-12 19:18:51 +0530

    create app

---
+++ osmdine food app/FoodOrder/app/src/main/java/com/example/foodapp/food_order/Interface/ItemClickListener.java
@@ -0,0 +1,7 @@
+package com.example.foodapp.food_order.Interface;
+
+import android.view.View;
+
+public interface ItemClickListener {
+    void onclick(View view, int position, boolean isLongClick);
+}
